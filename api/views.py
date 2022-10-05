89732e7e5f4afa4a87173787caef5a47215f5a86
OleksiiKukla <107278869+OleksiiKukla@users.noreply.github.com>
2022-10-05 11:47:04 +0300

    Checking than user can't create more than 5 wallets

--- api/views.py
+++ api/views.py
@@ -1,10 +1,10 @@
-from rest_framework import generics
+from rest_framework import generics, status
 
 from rest_framework.response import Response
 
 
-from api.serializers import WalletSerializer
-from transactions.models import Wallet
+from api.serializers import WalletSerializer, TransactionSerializer
+from transactions.models import Wallet, Transaction
 
 
 class WalletListCreate(generics.ListCreateAPIView):
@@ -18,8 +18,47 @@ class WalletListCreate(generics.ListCreateAPIView):
         serializer = WalletSerializer(queryset, many=True)
         return Response(serializer.data)
 
+    def create(self, request, *args, **kwargs):
+        try:
+            if len(Wallet.objects.filter(user = request.user)) > 4:
+                raise ValueError
+        except ValueError:
+            return Response('You cant create more than 5 wallets')
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
 
 class WalletDetail(generics.RetrieveDestroyAPIView):
     lookup_field = "name"
     queryset = Wallet.objects.all()
     serializer_class = WalletSerializer
+
+
+
+class TransactionListCreate(generics.ListCreateAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset().filter(
+            user=request.user
+        )  # фильтрация по авторизованному пользователю
+        serializer = TransactionSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+     # def create(self, request, *args, **kwargs):
+     #    serializer = self.get_serializer(data=request.data)
+     #    serializer.is_valid(raise_exception=True)
+     #    self.perform_create(serializer)
+     #    headers = self.get_success_headers(serializer.data)
+     #    return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class TransactionDetail(generics.RetrieveAPIView):
+    lookup_field = "id"
+    queryset = Transaction.objects.all()
+    serializer_class = Transaction
